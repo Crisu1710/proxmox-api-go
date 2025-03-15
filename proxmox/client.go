2526de48c1ed94379bed98ad24298ce9bd8f9625
Crisu1710 <m2rs@posteo.de>
2025-03-15 13:49:18 +0100

    remove not required VmRef

--- proxmox/client.go
+++ proxmox/client.go
@@ -351,12 +351,8 @@ func (c *Client) GetStorageStatus(ctx context.Context, vmr *VmRef, storageName s
 	return
 }
 
-func (c *Client) GetStorageContent(ctx context.Context, vmr *VmRef, storageName string) (data map[string]interface{}, err error) {
-	err = c.CheckVmRef(ctx, vmr)
-	if err != nil {
-		return nil, err
-	}
-	url := fmt.Sprintf("/nodes/%s/storage/%s/content", vmr.node, storageName)
+func (c *Client) GetStorageContent(ctx context.Context, node string, storageName string) (data map[string]interface{}, err error) {
+	url := fmt.Sprintf("/nodes/%s/storage/%s/content", node, storageName)
 	err = c.GetJsonRetryable(ctx, url, &data, 3)
 	if err != nil {
 		return nil, err
